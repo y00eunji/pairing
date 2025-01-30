b44b9b21dc5da8b6378e012d0939a24dd1f37ccf
y00eunji <aine0227@naver.com>
2025-01-30 20:42:58 +0900

    setting: 프로젝트 기본 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  'prettier',
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off', // 사용하지 않는 변수가 있을 때 경고로 표시해요
+      '@typescript-eslint/no-explicit-any': 'warn', // any 타입을 사용할 때 경고로 표시해요
+    },
+  },
 ];
 
 export default eslintConfig;

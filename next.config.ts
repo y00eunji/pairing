956c6cc8f31e4df81d57625e5cc26c20197f919f
y00eunji <aine0227@naver.com>
2025-02-07 19:57:24 +0900

    feat: #30 SVG 컴포넌트 지원을 위한 webpack 설정 추가 (#31)

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,13 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
